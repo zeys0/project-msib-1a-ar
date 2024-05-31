a6d353a34c18adb0e049afc7bb896ce67357223e
zeys0 <muhammadrafli09.ml@gmail.com>
2024-05-31 21:48:32 +0700

    first commit

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("main/home.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run("0.0.0.0", port=8000, debug=True)
