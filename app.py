6126453ce7d092266b6d6f18e7868af91b78c91d
zeys0 <muhammadrafli09.ml@gmail.com>
2024-06-01 20:24:46 +0700

    menambah menu home dan kelola produk dashboard

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def hello():
     return render_template("main/home.html")
 
 
+@app.route("/admin")
+def adminHome():
+    return render_template("dashboard/HomeDash.html")
+
+
+@app.route("/kelolaproduk")
+def kelolaProduk():
+    return render_template("dashboard/kelolaProduk.html")
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8000, debug=True)
